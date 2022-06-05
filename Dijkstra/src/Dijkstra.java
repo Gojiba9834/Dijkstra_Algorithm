8b863834787b0a26299ec572701d6375398d739e
Gojiba9834 <79980502+Gojiba9834@users.noreply.github.com>
2022-06-05 13:50:02 +0900

    Dijkstra: Initial set up

---
+++ Dijkstra/src/Dijkstra.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Dijkstra {
+    public static void Dijkstra_Exec() throws IOException
+    {
+        System.out.println("To be updated this class.");
+    }
+}
